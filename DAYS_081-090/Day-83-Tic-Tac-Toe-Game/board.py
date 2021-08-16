1c31bcde7bbc72458257ca7949e98577aeee1d71
Mahamrithunjaya <sahasubhajitias59@gmail.com>
2021-08-16 13:20:15 +0530

    board structure complete

--- DAYS_081-090/Day-83-Tic-Tac-Toe-Game/board.py
+++ DAYS_081-090/Day-83-Tic-Tac-Toe-Game/board.py
@@ -14,6 +14,7 @@ class Board:
 """)
 
     def mark(self, loc):
+        """This will plot the location in the board according to the user's input"""
         if len(loc) == 2:
             row = int(loc[0] - 1)
             column = int(loc[1] - 1)
@@ -29,6 +30,7 @@ class Board:
             return False
 
     def check_player_x(self):
+        """This checks the winning positions for the player with X symbol and if it matches returns True"""
 
         # Checking Rows
         if self.board[0][0] == "❌" and self.board[0][1] == "❌" and self.board[0][2] == "❌":
@@ -55,6 +57,7 @@ class Board:
             return False
 
     def check_player_o(self):
+        """This checks the winning positions of the player with symbol O and if it matches returns True """
 
         # Checking Rows
         if self.board[0][0] == "⭕" and self.board[0][1] == "⭕" and self.board[0][2] == "⭕":
@@ -79,4 +82,31 @@ class Board:
             return True
         else:
             return False
-        
+
+    def check_draw(self):
+        """This checks whether the game is draw or not. Draw game means all the cells are filled but none of them are
+        in position to win."""
+        draw = True
+        for row in self.board:
+            for cell in row:
+                if cell == " ":
+                    draw = False
+        return draw
+
+    def switch_turn(self):
+        """This will switch the symbol for the player."""
+        if self.turn == "❌":
+            self.turn = "⭕"
+        else:
+            self.turn = "❌"
+
+    def check_winner(self):
+        """This will check the winner of the game."""
+        if self.check_player_x():
+            return True, "❌"
+        elif self.check_player_o():
+            return True, "⭕"
+        elif self.check_draw():
+            return True, "DRAW"
+        else:
+            return False, None
