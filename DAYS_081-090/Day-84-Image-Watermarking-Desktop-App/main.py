64c324748911cc9e1ba98e04a1546c90d92df21a
Mahamrithunjaya <sahasubhajitias59@gmail.com>
2023-08-01 06:50:14 +0530

    Packages are imported

---
+++ DAYS_081-090/Day-84-Image-Watermarking-Desktop-App/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import filedialog as fld
+from tkinter.colorchooser import askcolor
+import tkinter.messagebox
+import PIL
+from PIL import Image, ImageDraw, ImageFont, ImageTk, ImageColor
+import matplotlib
+from matplotlib import font_manager
+import os
+
